ee8d0ee24675cb2663799193b394190830fc3fe5
Valera <valera.gs@bk.ru>
2023-02-07 18:48:48 +0300

    Replaced api project.

--- SportLookup.Backend/Frameworks/SportLookup.Backend.WebApi/Program.cs
+++ SportLookup.Backend/Frameworks/SportLookup.Backend.WebApi/Program.cs
@@ -1,22 +1,62 @@
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
-ConfigureServices(builder.Services);
+ConfigureServices(builder.Services, builder.Environment);
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-    app.UseDeveloperExceptionPage();
 
-app.UseRouting();
+app.UseCors(cfg =>
+{
+    cfg.WithOrigins("http://localhost:3000")
+        .AllowAnyHeader()
+        .AllowAnyMethod()
+        .AllowCredentials();
+});
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI(cfg =>
+    {
+        cfg.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    });
+}
+
+
+app.UseHttpsRedirection();
 
 app.UseAuthentication();
 app.UseAuthorization();
 
-app.UseEndpoints(endpoints => endpoints.MapDefaultControllerRoute());
+app.MapControllers();
 
 app.Run();
 
-static void ConfigureServices(IServiceCollection services)
+static void ConfigureServices(IServiceCollection services, IWebHostEnvironment env)
 {
-    services.AddControllersWithViews();
+    services.AddCors();
+
+    services.AddAuthentication("Bearer")
+    .AddJwtBearer("Bearer", cfg =>
+    {
+        cfg.Authority = "https://localhost:5001";
+        //TODO: Add UI client check.
+
+        if (env.IsDevelopment())
+            cfg.RequireHttpsMetadata = false;
+
+        cfg.Audience = "main-sport-lookup-api-resource";
+    });
+    services.AddAuthorization();
+
+    services.AddControllers();
+    services.AddEndpointsApiExplorer();
+    services.AddSwaggerGen(cfg =>
+    {
+        string docXMLFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+        cfg.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, docXMLFilename));
+    });
 }
