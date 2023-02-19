3b0e0687f79c888da42c5e361ee6a731c411685d
Valera <valera.gs@bk.ru>
2023-02-20 02:18:29 +0300

    Extracted infrastructure implementation to separeted proj. Connected MediatR requests with Fluent validation.

---
+++ SportLookup.Backend/UseCases/SportLookup.Backend.Infrastructure.Implementation/InfrastructureModule.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SportLookup.Backend.Infrastructure.Implementation.PipelineHandlers;
+
+namespace SportLookup.Backend.Infrastructure.Implementation;
+
+public class InfrastructureModule : Utils.Module
+{
+    public override void Register(IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationPipelineHandler<,>));
+    }
+}
---
+++ SportLookup.Backend/UseCases/SportLookup.Backend.Infrastructure.Implementation/PipelineHandlers/ValidationPipelineHandler.cs
@@ -0,0 +1,31 @@
+﻿using FluentValidation;
+using MediatR;
+
+namespace SportLookup.Backend.Infrastructure.Implementation.PipelineHandlers;
+
+public class ValidationPipelineHandler<TRequest, TResponce> : IPipelineBehavior<TRequest, TResponce>
+    where TRequest : IRequest<TResponce>
+{
+    private readonly IEnumerable<IValidator<IRequest>> _validators;
+
+    public ValidationPipelineHandler(IEnumerable<IValidator<IRequest>> validators)
+    {
+        _validators = validators;
+    }
+
+    public Task<TResponce> Handle(TRequest request, RequestHandlerDelegate<TResponce> next, CancellationToken cancellationToken)
+    {
+        ValidationContext<TRequest> validationContext = new(request);
+
+        var fails = _validators
+            .Select(vr => vr.Validate(validationContext))
+            .Where(vRes => !vRes.IsValid)
+            .SelectMany(vRes => vRes.Errors)
+            .ToList();
+
+        if (fails.Any())
+            throw new ValidationException(fails);
+
+        return next.Invoke();
+    }
+}
